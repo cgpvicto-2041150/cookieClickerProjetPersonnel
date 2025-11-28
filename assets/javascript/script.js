25ccfac4eaeaa8377b7990889f78fd5e52201149
cgpvicto-2041150 <2041150@etudiant.cegepvicto.ca>
2025-11-28 11:52:27 -0500

    boutique(js)

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,13 +1,45 @@
 let biscuitsTotal = 0;
+let parsecondeNb = 0;
+let parclicNb = 1;
+let seconde = 0;
+
 const compteur = document.getElementsByClassName("pro")[0];
+
 const biscuit = document.getElementsByClassName("biscuit-btn")[0];
-const curseur = document.getElementById("souris");
+const curseur = document.getElementById("curseur");
 const souris = document.getElementById("souris");
 
-
+const parSeconde = document.getElementById("parSeconde");
+const parClic = document.getElementById("parClic");
+const prixNbClic = document.getElementById("prixNbClic");
+const prixNbCurseur = document.getElementById("prixNbCurseur");
 
 biscuit.addEventListener("click", () =>{
-    biscuitsTotal++;
+    biscuitsTotal+=parclicNb;
     compteur.innerHTML = biscuitsTotal + " biscuits"; 
 });
 
+//Achat curseur
+curseur.addEventListener("click", () =>{
+    if(parsecondeNb<=biscuitsTotal){
+        parsecondeNb++;
+        parSeconde.innerHTML = "par seconde : " + parsecondeNb;
+        prixNbCurseur.innerHTML = " "+ parsecondeNb + " biscuit(s)";
+    }
+    
+});
+
+//Achat souris
+souris.addEventListener("click", () =>{
+    if(parclicNb<=biscuitsTotal){
+        parclicNb++;
+        parClic.innerHTML = "par click : " + parclicNb;
+        prixNbClic.innerHTML = " " + parclicNb + " biscuit(s)";
+    }
+    
+});
+
+setInterval(()=>{
+    biscuitsTotal += parsecondeNb;
+    compteur.innerHTML = biscuitsTotal +" biscuits"
+}, 1000);
